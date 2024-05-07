25ed717def59b01959999f1992aaa2156547157a
tardinger <145486601+tardinger@users.noreply.github.com>
2024-05-07 13:53:32 -0700

    ndfnje

--- js/pageTwo.js
+++ js/pageTwo.js
@@ -11,7 +11,7 @@ function preload(){
   mercedes = loadImage('img/mercedes.png');
   monaLisa = loadImage('img/monaLisa.jpg');
   tickets = loadImage('img/tickets.png');
-  mainFont = loadFont('img/JacquesFrancois-Regular.ttf')
+  mainFont = loadFont('img/JacquesFrancois-Regular.ttf');
 }
 
 function setup() {
@@ -23,24 +23,24 @@ function setup() {
 }
 
 function draw() {
-  background(228, 100, 35);
-  image(eiffelTower, width/3.5, height/2.25, 650, 650);
+  background(241, 100, 79);
+  image(eiffelTower, width/3, height/2.25, 650, 650);
   
   map(mappedXPosition, 0, width, 50, 100);
   image(mercedes, mouseX, 730, 470, 400)
   
   textSize(40);
-  fill(228, 100, 35)
-  text("Screaming through the streets of Paris in a chauffeur-driven Mercedes ", width/2, 750)
+  fill(241, 100, 79);
+  text("Screaming through the streets of Paris in a chauffeur-driven Mercedes ", width/2, 750);
   
   if (mouseX > 1400){
-    image(monaLisa, 1020,330,330,390)
-    fill(220)
-    text('On the way to see the Mona Lisa in the Louvre with my daughter, Kimberly.',width/2,60)
+    image(monaLisa, 1020,330,330,390);
+    fill(220);
+    text('On the way to see the Mona Lisa in the Louvre with my daughter, Kimberly.',width/2,60);
   }
   
   if (mouseX > 1200 && !linkIsMade){
-    let link = createA('pagethree.html', '<img src="img/tickets.png"/>')
+    let link = createA('pagethree.html', '<img src="img/tickets.png"/>');
     link.id('tickets');
     linkIsMade = true;
   }  
